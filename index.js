3fe1b632a77eee1030aeb13cb7e58faa91f8166b
Abdulahad <abdulahadinamovyt@gmail.com>
2024-09-15 22:29:20 +0500

    first figma

---
+++ index.js
@@ -0,0 +1,184 @@
+const translations = {
+    en: {
+        about: "About Us",
+        courses: "Courses",
+        career: "Career",
+        reviews: "Reviews",
+        branches: "Branches",
+        bannertitle: "How and where to learn English easily and simply?",
+        bannerdescription: "Thousands of students achieve amazing results. They start speaking English even at the initial stages. If they could do it, so can you!",
+        register: "Register",
+        learnwithus: "With us, you will learn:",
+        content1: "Speak confidently and clearly with minimal or no accent",
+        content2: "30+ new words in your vocabulary daily",
+        content3: "30+ new words in your vocabulary daily",
+        content4: "How to build a habit of repeating words every hour",
+        content5: "30+ new words in your vocabulary daily",
+        content6: "30+ new words in your vocabulary daily",
+        ourteachers: "Our Teachers",
+        reviews: "Reviews",
+        readmore: "Read More",
+        send: "Send",
+        footerrights: "All rights reserved.",
+        footercopyright: "© NEW OXFORD 2024.",
+        footerabout: "About Us",
+        footercourses: "Courses",
+        footercareer: "Career",
+        footerbranches: "Branches",
+        footerfaq: "FAQ",
+        footerblog: "Blog",
+        footerterms: "Terms of Service",
+        footercookies: "Cookie Settings",
+        footerenglish: "English Language",
+        footerrussian: "Russian Language",
+        footerdtm: "DTM",
+        contactquestion1: "Can the lesson recording be used by multiple people?",
+        contactanswer1: "No, the recording is intended for one person only. Distribution is prohibited.",
+        contactquestion2: "Can I pay in installments?",
+        contactanswer2: "Yes, but with mandatory marking by the administrator",
+        contactquestion3: "What IELTS score is needed for work?",
+        contactanswer3: "You need at least IELTS 6+ to comfortably communicate with others.",
+        contactquestion4: "What if I fail the exam?",
+        contactanswer4: "Your teacher will set a retake date. If you fail again, the training will stop until the next payment."
+    },
+    es: {
+        about: "О нас",
+        courses: "Курсы",
+        career: "Карьера",
+        reviews: "Отзывы",
+        branches: "Филиалы",
+        bannertitle: "Как и где выучить  Английский язык, что бы было легко и просто?",
+        bannerdescription: "Уже более десятки тысяч студентов получают космические <br> результаты. Начинают говорить на  английском ещё будучи на <br> начальных этапах. Смогли они - сможете и вы!",
+        register: "Зарегистрироваться",
+        learnwithus: "С нами вы обучитесь:",
+        content1: "Разговаривать уверенно, четко  каждую букву,  минимальным или вовсе без акцента",
+        content2: "30+ Hовых слов в ваш словарный запас ежедневно",
+        content3: "30+ Hовых слов в ваш словарный  запас ежедневно",
+        content4: "Как встроить привычку  повторять слова каждый час",
+        content5: "30+ Новых слов в ваш словарный  запас ежедневно",
+        content6: "30+ Новых слов в ваш словарный запас ежедневно",
+        ourteachers: "Наши учителя",
+        reviews: "Отзывы",
+        readmore: "Читать полностью",
+        send: "Отправить",
+        footerrights: "Все права защищены.",
+        footercopyright: "© NEW OXFORD 2024.",
+        footerabout: "О нас",
+        footercourses: "Курсы",
+        footercareer: "Карьера",
+        footerbranches: "Филиалы",
+        footerfaq: "FAQ",
+        footerblog: "Блог",
+        footerterms: "Пользовательское соглашение",
+        footercookies: "Настрйоки cookie",
+        footerenglish: "Английский язык",
+        footerrussian: "Русский язы",
+        footerdtm: "ДТМ",
+        contactquestion1: "Могут ли, использовать запись урока несколько людей?",
+        contactanswer1: "запись предназначена для одного человека Распространение - запрещено.",
+        contactquestion2: "Можно платить частями?",
+        contactanswer2: "Можно, но с обязательной отметкой у администратора.",
+        contactquestion3: "Какой IELTS нужен для работы?",
+        contactanswer3: "Вам нужен как минимум IELTS 6+ что бы комфортно вести коммуникацию с другими людьми",
+        contactquestion4: "Что если я не сдал экзамен?",
+        contactanswer4: "Ваш учитель назовет дату пересдачи. Если и на этом экзамене <br> вы не сдадите - обучение останавливается до след. оплаты."
+    },
+    fr: {
+        about: "Biz haqimizda",
+        courses: "Kurslar",
+        career: "Karyera",
+        reviews: "Sharhlar",
+        branches: "Filiallar",
+        bannertitle: "Ingliz tilini qanday va qayerda o'rganish kerak, nima oson va sodda bo'lar edi?",
+        bannerdescription: "O'n mingdan ortiq talabalar kosmik natijalarni olishmoqda. Ular hali boshlang'ich bosqichlarida ingliz tilida gaplasha boshlaydilar. Agar ular buni qila olsa, siz ham qila olasiz!",
+        register: "Roʻyxatdan oʻtish",
+        learnwithus: "Biz bilan siz quyidagilarni o'rganasiz:",
+        content1: "Ishonch bilan, har bir harfni minimal yoki aksentsiz aniq gapiring",
+        content2: "Har kuni lug'atingizga qo'shiladigan 30+ yangi so'zlar",
+        content3: "Har kuni lug'atingizga qo'shiladigan 30+ yangi so'zlar",
+        content4: "Qanday qilib har soatda so'zlarni takrorlash odatini shakllantirish mumkin",
+        content5: "Har kuni lug'atingizga qo'shiladigan 30+ yangi so'zlar",
+        content6: "Har kuni lug'atingizga qo'shiladigan 30+ yangi so'zlar",
+        ourteachers: "Bizning o'qituvchilarimiz",
+        reviews: "Sharhlar",
+        readmore: "Ko'proq o'qish",
+        send: "Yuborish",
+        footerrights: "Barcha huquqlar himoyalangan.",
+        footercopyright: "© NEW OXFORD 2024.",
+        footerabout: "Biz haqimizda",
+        footercourses: "Kurslar",
+        footercareer: "Karyera",
+        footerbranches: "Filiallar",
+        footerfaq: "TSS",
+        footerblog: "Blog",
+        footerterms: "Polzovatelskoe ommaviy axborot vositalari",
+        footercookies: "Cookie sozlamalari",
+        footerenglish: "Ingliz tili",
+        footerrussian: "rus tili",
+        footerdtm: "DTM",
+        contactquestion1: "Kurs videosidan bir nechta odam foydalanishi mumkinmi?",
+        contactanswer1: "Yo'q, yozuv faqat bir kishi uchun mo'ljallangan. Tarqatish taqiqlangan.",
+        contactquestion2: "Bir necha bo'lib to'lashim mumkinmi?",
+        contactanswer2: "Ha, lekin administrator tomonidan majburiy belgi bilan.",
+        contactquestion3: "Ishlash uchun qancha IELTS ball kerak?",
+        contactanswer3: "Boshqalar bilan qulay muloqot qilish uchun sizga kamida 6+ IELTS ball kerak.",
+        contactquestion4: "Agar imtihondan o'ta olmasam nima bo'ladi?",
+        contactanswer4: "O'qituvchingiz qayta boshlash sanasini belgilaydi. Agar siz yana muvaffaqiyatsizlikka uchrasangiz, keyingi to'lovgacha trening to'xtatiladi."
+    }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    const languageSelector = document.getElementById('language-selector');
+    languageSelector.addEventListener('change', (event) => {
+        const language = event.target.value;
+        translatePage(language);
+    });
+});
+
+function translatePage(language) {
+    document.querySelectorAll('[data-key]').forEach((element) => {
+        const key = element.getAttribute('data-key');
+        if (translations[language] && translations[language][key]) {
+            element.textContent = translations[language][key];
+        }
+    });
+}
+let isBold = false; // Qoraytirilganligini kuzatadi
+let isItalic = false; // O'qaytirilganligini kuzatadi
+
+const textElement = document.getElementById('text');
+const toggleBtn = document.getElementById('toggleBtn');
+
+toggleBtn.addEventListener('click', function() {
+  if (!isBold && !isItalic) {
+    // Birinchi bosishda matnni qoraytirish
+    textElement.style.fontWeight = 'bold';
+    isBold = true;
+  } else if (isBold && !isItalic) {
+    // Ikkinchi bosishda o'qaytirish
+    textElement.style.fontWeight = 'normal'; // Qoraytirishni o'chirish
+    textElement.style.fontStyle = 'italic'; // O'qaytirish
+    isItalic = true;
+  } else if (isBold && isItalic) {
+    // Uchinchida yana normal holatga qaytadi
+    textElement.style.fontWeight = 'normal';
+    textElement.style.fontStyle = 'normal';
+    isBold = false;
+    isItalic = false;
+  }
+});
+const toggle = document.getElementById('toggleDark');
+const body = document.querySelector('body');
+
+toggle.addEventListener('click', function(){
+    this.classList.toggle('bi-moon');
+    if(this.classList.toggle('bi-brightness-high-fill')){
+        body.style.background = 'white';
+        body.style.color = 'black';
+        body.style.transition = '2s';
+    }else{
+        body.style.background = 'black';
+        body.style.color = 'white';
+        body.style.transition = '2s';
+    }
+});
